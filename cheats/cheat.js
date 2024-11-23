b5452604739760548b23dd55bbee5f18d74b7b83
Saviru Kashmira Atapattu <savirukashmira@gmail.com>
2024-11-23 13:31:13 +0530

    Update cheat.js

--- cheats/cheat.js
+++ cheats/cheat.js
@@ -1,10 +1,10 @@
 const textArray = ["Kiwwa neda ada open karanna epa kiyala. Heta enna.", "Ada epa chikuuuuuuu. Heta open karanna. Surprise ekak.", "Adai hetai kiyana eke wenasa dannawada ? Poddak iwasanna.", "Ada open karala wadak na ithin. Poddak idapnko heta wenakan. heta uden aharuna gmn oopen karanna.", "Bane ithin karanna. ada awata wadak na. Ubt mnwth ballana ba chikuu."];
 const imageArray = [
-    "1.webp",
-    "2.webp",
-    "3.webp",
-    "4.webp",
-    "5.webp"
+    "cheats/1.webp",
+    "cheats/2.webp",
+    "cheats/3.webp",
+    "cheats/4.webp",
+    "cheats/5.webp"
 ];
 
 function changeTextRandomly() {
